94f293cf222f02fddce812708324f322ff5cf4fd
Aditya Tippanaboena <aditya4pavan@gmail.com>
2024-09-18 02:37:47 -0400

    Add Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,4 @@
-import axios from "axios";
 
-const getTodo = async () => {
-  const response = await axios.get("https://jsonplaceholder.typicode.com/todos/1");
-  return response.data;
-}
-
-export default async function App() {
-  const data = await getTodo();
-  return <main>{JSON.stringify(data)}</main>;
+export default function Home() {
+  return 'Dashboard Coming Soon';
 }
